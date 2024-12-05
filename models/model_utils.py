d16b67fa82b9eeb2556400d49fded9a61e3cbbd7
ViniCS2001 <ballaervin24@gmail.com>
2024-12-05 02:05:26 +0100

    Changed model utils file

--- models/model_utils.py
+++ models/model_utils.py
@@ -1,6 +1,8 @@
 import joblib
 from .text_pre_processor import preprocess_text
 
+#model path = "/Users/ervinballa/Desktop/P_AI_Eng/ws2024-principles-of-ai-engineering/trained_models/model2.pkl"
+#vector path = "/Users/ervinballa/Desktop/P_AI_Eng/ws2024-principles-of-ai-engineering/vectorizers/vectorizer10k2.pkl"
 def load_model_and_vectorizer():
     """
     Load the pre-trained model and vectorizer from disk.
@@ -9,8 +11,8 @@ def load_model_and_vectorizer():
         model: The trained classification model.
     """
     try:
-        vectorizer = joblib.load("/Users/ervinballa/Desktop/P_AI_Eng/ws2024-principles-of-ai-engineering/vectorizers/vectorizer10k2.pkl")
-        model = joblib.load("/Users/ervinballa/Desktop/P_AI_Eng/ws2024-principles-of-ai-engineering/trained_models/model2.pkl")
+        vectorizer = joblib.load("vectorizer10k2.pkl")
+        model = joblib.load("model2.pkl")
         return vectorizer, model
     except Exception as e:
         raise RuntimeError(f"Error loading model or vectorizer: {e}")
